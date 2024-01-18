2c3616870d2a18bc455a741220827ad4cd8ce23e
N1N3T4L3S <6765614+ninetales@users.noreply.github.com>
2024-01-18 22:14:50 +0100

    :sparkles: login: added form handling (need some more validation)
    added fundamental files
    
    updated the folder strucure and naming convention

--- js/app.js
+++
@@ -1,5 +0,0 @@
-function initPage() {
-    console.log('Hello World');
-}
-
-document.addEventListener('DOMContentLoaded', initPage);
---
+++ utilities/config.js
@@ -0,0 +1,4 @@
+export const settings = {
+    DB_USER_PATH: 'http://localhost:3000/users',
+    DB_COURSE_PATH: 'http://localhost:3000/courses',
+}
